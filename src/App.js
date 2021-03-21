505cfe9dade7eadc6cce3cbca36d12a96c8d8bdc
unknown <leonel01sys@gmail.com>
2021-03-20 19:35:11 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+import { 
+  LoginButton,
+  LogoutButton,
+  Text,
+  useSession,
+  CombinedDataProvider 
+} from '@inrupt/solid-ui-react';
+import { useState } from 'react';
+
+const authOptions = {
+  clientName: 'Lista de Tareas - Solid'
+};
 
 function App() {
+  const { session } = useSession();
+  const [oidcIssuer, setOidcIssuer] = useState("");
+  const handleChange = (event) => {
+    setOidcIssuer(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      {session.info.isLoggedIn ? (
+        <CombinedDataProvider 
+          datasetUrl={session.info.webId}
+          thingUrl={session.info.webId}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div>
+            <span>Iniciaste sesión como: </span>
+            <Text 
+              properties={[
+              "http://www.w3.org/2006/vcard/ns#fn",
+              "http://xmlns.com/foaf/0.1/name",]}
+            />
+            <LogoutButton />
+          </div>
+        </CombinedDataProvider>
+      ) : 
+      (
+      <div>
+        <span>
+          Inicia Sesión con:
+          <input 
+            className="oidc-issuer-input"
+            type="text"
+            name="oidcIssuer"
+            list="providers"
+            value={oidcIssuer}
+            onChange={handleChange}
+          />
+          <datalist id="providers">
+            <option value="https://broker.pod.inrupt.com/" />
+            <option value="https://inrupt.net/" />
+          </datalist>
+        </span>
+        <LoginButton 
+          oidcIssuer={oidcIssuer}
+          redirectUrl={window.location.href}
+          authOptions={authOptions}
+        />
+      </div>
+    )}
+  </div>
   );
 }
 
